720605faa24ca00f3c5779299362f6ba2f8accfc
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-23 17:29:48 +0530

    adding emoji

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,37 +1,55 @@
-import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
+import * as React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
  
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
+import MovieFilterIcon from '@mui/icons-material/MovieFilter';
+import TvIcon from '@mui/icons-material/Tv';
+import SearchIcon from '@mui/icons-material/Search';
+import WhatshotIcon from "@mui/icons-material/Whatshot";
 
 export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
+  const [value, setValue] = React.useState("recents");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <BottomNavigation sx={{ width: "100%", position:"fixed", bottom:0,bgcolor:"#2d313a" , zIndex:100 }} value={value} onChange={handleChange}>
+    <BottomNavigation
+      sx={{
+        width: "100%",
+        position: "fixed",
+        bottom: 0,
+        bgcolor: "#2d313a",
+        zIndex: 100,
+      }}
+      value={value}
+      onChange={handleChange}
+    >
       <BottomNavigationAction
+        style={{ color: "white" }}
         label="Trendings"
         value="trendings"
         icon={<WhatshotIcon />}
       />
       <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
+        style={{ color: "white" }}
+        label="Movies"
+        value="movies"
+        icon={<MovieFilterIcon />}
       />
       <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
+        style={{ color: "white" }}
+        label="TV Series"
+        value="tvSeries"
+        icon={<TvIcon />}
       />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search "
+        value="search"
+        icon={<SearchIcon />}
+      />
     </BottomNavigation>
   );
 }
