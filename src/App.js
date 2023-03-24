2589e639fd46d0a9cc2054e40fa82403f4dc9c92
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-24 09:48:12 +0530

    some functionitly add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { Switch } from "@mui/material";
+import { Container } from "@mui/system";
+import { BrowserRouter , Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
-import SimpleBottomNavigation from "./components/MainNav"
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
 function App() {
   return (
-    <>
-       <Header />
-      <div className="app"></div>
-      <SimpleBottomNavigation/>
-     
-      
-    </>
+    <BrowserRouter>
+      <Header />
+      <div className="app">
+        <Container> 
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,14 +1,28 @@
 import * as React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
- 
+import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieFilterIcon from '@mui/icons-material/MovieFilter';
 import TvIcon from '@mui/icons-material/Tv';
 import SearchIcon from '@mui/icons-material/Search';
-import WhatshotIcon from "@mui/icons-material/Whatshot";
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom' ;
 
 export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState("recents");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (value === 0) {
+      navigate.push("/");
+    } else if (value === 1) {
+      navigate.push("/movies");
+    } else if (value === 2) {
+      navigate.push("/series");
+    } else if (value === 3) {
+      navigate.push("/search");
+    }
+  }, [value, navigate]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -26,30 +40,31 @@ export default function LabelBottomNavigation() {
       value={value}
       onChange={handleChange}
     >
+      {" "}
       <BottomNavigationAction
         style={{ color: "white" }}
-        label="Trendings"
+        label="Trending"
         value="trendings"
         icon={<WhatshotIcon />}
-      />
+      />{" "}
       <BottomNavigationAction
         style={{ color: "white" }}
         label="Movies"
         value="movies"
         icon={<MovieFilterIcon />}
-      />
+      />{" "}
       <BottomNavigationAction
         style={{ color: "white" }}
         label="TV Series"
         value="tvSeries"
         icon={<TvIcon />}
-      />
+      />{" "}
       <BottomNavigationAction
         style={{ color: "white" }}
         label="Search "
         value="search"
         icon={<SearchIcon />}
-      />
+      />{" "}
     </BottomNavigation>
   );
 }
